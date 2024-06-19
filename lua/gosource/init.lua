a16bb69a4bb664c965f83ad6f2da4f8e5ce0ee71
schetvkomplax <schet.v.komplax@gmail.com>
2024-06-19 19:08:44 +0300

    add create option

--- lua/gosource/init.lua
+++ lua/gosource/init.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 local function jumpto(filename)
 	vim.cmd('e ' .. filename)
 end
@@ -5,34 +7,27 @@ end
 local function change_extension(patterns)
 	local filename = vim.api.nvim_buf_get_name(0)
 	for _, p in ipairs(patterns) do
-		local k, v = unpack(p)
+		local k, v, create = p[1], p[2], p['create']
+
 		if filename:sub(-string.len(k)) ~= k then
 			goto continue
 		end
-		local new = ''
-		if type(v) == 'string' then
-			v = {v}
-		end
-		for _, suffix in ipairs(v) do
-			new = filename:sub(0, -string.len(k) - 1) .. suffix
-			if vim.fn.filereadable(new) ~= 0 then
-				jumpto(new)
-				return
-			end
-		end
-		if new ~= '' then
+
+		local new = filename:sub(0, -string.len(k) - 1) .. v
+		if vim.fn.filereadable(new) ~= 0 or create == true then
 			jumpto(new)
 			return
 		end
+
 		::continue::
 	end
 end
 
-return {
-	setup = function () end,
-	change_extension = function (patterns)
-		return function()
-			return change_extension(patterns)
-		end
-	end,
-}
+M.setup = function () end
+M.change_extension = function (patterns)
+	return function()
+		return change_extension(patterns)
+	end
+end
+
+return M
